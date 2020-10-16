81793f43bec5d838e8f0f0b6aaadb88561dde4bf
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-10-15 22:59:17 -0300

    refactor

--- Routes/products.js
+++ Routes/products.js
@@ -34,12 +34,14 @@ router.get('/:id', (req, res)=>{
     })
 })
 router.patch('/:id', (req, res)=>{
-    Products.findByIdAndUpdate(req.params.id, {name: req.body.name, price: req.body.price }).then(result =>{
+    Products.update({_id: req.params.id}, {$set: {name: req.body.name, price: req.body.price }}).then(result =>{
         res.status(200).json(result)
+    }).catch((err)=>{
+        res.status(500).json(err)
     })
 })
 router.delete('/:id', (req, res)=>{
-    Products.findByIdAndDelete(req.params.id).then(result =>{
+    Products.remove({_id: req.params.id}).then(result =>{
         res.status(200).json(result)
     }).catch((err)=>{
         res.status(500).json(err)
